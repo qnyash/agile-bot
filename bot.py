0c96321bc8d17de0c25665c10e13d540d6bfa501
qnyash <tisunceritunce@gmail.com>
2025-11-18 16:38:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -261,3 +261,16 @@ async def on_startup(dp_):
 # ======================
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+
+import asyncio
+from aiohttp import web
+
+async def handler(request):
+    return web.Response(text="OK")
+
+app = web.Application()
+app.router.add_get("/", handler)
+
+# запускаем сервер на произвольном порту
+port = int(os.environ.get("PORT", 10000))
+web.run_app(app, port=port)
